d006a3c8704eb961d2db63e40d8bd5387b93374a
ceciliahangzhou <cezhou@deloitte.ca>
2023-07-17 14:04:48 -0400

    add services file

---
+++ src/apiManager/httpRequestHandler/index.js
@@ -0,0 +1,96 @@
+import axios from 'axios';
+// import UserService from 'services/userService';
+
+// GET request example
+const GetRequest = (url, params = {}, headers = {}) => {
+    return axios.get(url, {
+        params: params,
+        headers: {
+            // 'Content-type': 'application/json',
+            // Authorization: `Bearer ${UserService.getToken()}`,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+            // ...headers,
+        },
+    });
+};
+
+// POST request example
+const PostRequest = (url, data = {}, params = {}) => {
+    return axios.post(url, data, {
+        params,
+        headers: {
+            // 'Content-type': 'application/json',
+            // Authorization: `Bearer ${UserService.getToken()}`,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+        },
+    });
+};
+
+// PUT request example
+const PutRequest = (url, data = {}, params = {}) => {
+    return axios.put(url, data, {
+        params,
+        headers: {
+            // 'Content-type': 'application/json',
+            // Authorization: `Bearer ${UserService.getToken()}`,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+        },
+    });
+};
+
+// PATCH request example
+const PatchRequest = (url, data = {}) => {
+    return axios.patch(url, data, {
+        headers: {
+            // 'Content-type': 'application/json',
+            // Authorization: `Bearer ${UserService.getToken()}`,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+        },
+    });
+};
+
+// DELETE request example
+const DeleteRequest = (url, params = {}) => {
+    return axios.delete(url, {
+        params: params,
+        headers: {
+            // 'Content-type': 'application/json',
+            // Authorization: `Bearer ${UserService.getToken()}`,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+        },
+    });
+};
+
+
+export const OSSGetRequest = (url, requestOptions) => {
+    return axios.get(url, {
+        headers: {
+            // 'X-Amz-Date': requestOptions.amzDate,
+            // Authorization: requestOptions.authHeader,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+        },
+        responseType: 'blob',
+    });
+};
+
+export const OSSPutRequest = (url, data, requestOptions) => {
+    return axios.put(url, data, {
+        headers: {
+            // 'X-Amz-Date': requestOptions.amzDate,
+            // Authorization: requestOptions.authHeader,
+            // 'tenant-id': `${sessionStorage.getItem('tenantId')}`,
+        },
+    });
+};
+
+const HttpRequest = {
+    GetRequest,
+    PostRequest,
+    PutRequest,
+    PatchRequest,
+    DeleteRequest,
+    OSSGetRequest,
+    OSSPutRequest,
+}
+
+export default HttpRequest;
---
+++ src/config.js
@@ -0,0 +1,29 @@
+import { hasKey } from 'utils';
+
+// adding localStorage to access the MET API from external sources(eg: web-components)
+const API_URL = localStorage.getItem('met-api-url') || getEnv('REACT_APP_API_URL');
+const PUBLIC_URL = localStorage.getItem('met-public-url') || getEnv('REACT_APP_PUBLIC_URL');
+// const REDASH_DASHBOARD_URL = getEnv('REACT_APP_REDASH_PUBLIC_URL');
+// const REDASH_CMNTS_DASHBOARD_URL = getEnv('REACT_APP_REDASH_COMMENTS_PUBLIC_URL');
+
+// Keycloak Environment Variables
+const KC_URL = REACT_APP_API_URL; 
+const KC_CLIENT = REACT_APP_KEYCLOAK_CLIENT;
+const KC_REALM = REACT_APP_KEYCLOAK_REALM;
+const KC_ADMIN_ROLE = REACT_APP_KEYCLOAK_ADMIN_ROLE;
+
+const AppConfig = {
+    apiUrl: API_URL,
+    publicUrl: PUBLIC_URL,
+    keycloak: {
+        url: KC_URL || '',
+        clientId: KC_CLIENT || '',
+        realm: KC_REALM || '',
+        adminRole: KC_ADMIN_ROLE || 'admin',
+    },
+    constants: {
+        engagementProjectTypes: ENGAGEMENT_PROJECT_TYPES,
+    },
+};
+
+module.exports = AppConfig
