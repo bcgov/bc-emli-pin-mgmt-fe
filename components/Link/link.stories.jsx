e1fbf4a8cec8a852622f6101bcb906203531f6a5
emilymendelson <emendelson@deloitte.ca>
2023-07-28 09:36:24 -0400

    clean up

--- components/Link/link.stories.jsx
+++ components/Link/link.stories.jsx
@@ -1,12 +1,12 @@
 import Link from './Link'
 
 export default {
-	title: 'Link',
-	component: Link,
-	args: {
-		linkId: 'btnBasic',
-		href: 'https://openai.com/blog/chatgpt',
-	},
+    title: 'Link',
+    component: Link,
+    args: {
+        linkId: 'btnBasic',
+        href: 'https://www.google.com/',
+    },
 }
 
 const Template = (args) => <Link {...args}>Click me!</Link>
@@ -15,25 +15,25 @@ export const PrimaryTemplate = Template.bind({})
 
 export const DangerTemplate = Template.bind({})
 DangerTemplate.args = {
-	variant: 'danger',
+    variant: 'danger',
 }
 
 export const WarningTemplate = Template.bind({})
 WarningTemplate.args = {
-	variant: 'warning',
+    variant: 'warning',
 }
 
 export const SecondaryTemplate = Template.bind({})
 SecondaryTemplate.args = {
-	variant: 'secondary',
+    variant: 'secondary',
 }
 
 export const SuccessTemplate = Template.bind({})
 SuccessTemplate.args = {
-	variant: 'success',
+    variant: 'success',
 }
 
 export const DisabledTemplate = Template.bind({})
 DisabledTemplate.args = {
-	variant: 'disabled',
+    variant: 'disabled',
 }
