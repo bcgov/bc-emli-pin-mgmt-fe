46896d4af54bf02089de285255108a8a36739265
emilymendelson <emendelson@deloitte.ca>
2023-07-28 09:37:02 -0400

    clean up

--- components/Link/Link.test.jsx
+++ components/Link/Link.test.jsx
@@ -1,5 +1,3 @@
-// /* eslint-disable testing-library/prefer-screen-queries */
-// /* eslint-disable testing-library/no-node-access */
 import { render } from '@testing-library/react'
 import { composeStories } from '@storybook/testing-react'
 
@@ -8,9 +6,9 @@ import * as stories from './Link.stories'
 const { PrimaryTemplate } = composeStories(stories)
 
 describe('<Link />', () => {
-	it('should render properly', () => {
-		const { container } = render(<PrimaryTemplate />)
+    it('should render properly', () => {
+        const { container } = render(<PrimaryTemplate />)
 
-		expect(container.firstChild).toBeTruthy()
-	})
+        expect(container.firstChild).toBeTruthy()
+    })
 })
