59ca5adca4d595139b497009be6d41f7a5da7d9f
emilymendelson <emendelson@deloitte.ca>
2023-07-28 09:46:34 -0400

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
-import '../components/Link/styles.css'
-import '../components/Radio Button/styles.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
