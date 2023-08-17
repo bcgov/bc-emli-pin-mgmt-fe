ed21f6ca09f3039d0bd5a50278f5a334cb572e85
emilymendelson <emendelson@deloitte.ca>
2023-08-17 09:45:47 -0400

    Adding in searchresult component and styling

---
+++ components/Search Results/SearchResults.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types'
+
+import styles from './SearchResults.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import AddressCard from '../Address Card/index'
+
+export default function SearchResults({ searchString }) {
+    if (searchString) {
+        const [results, setResults] = useState(null)
+
+        let address = searchString?.toLowerCase()
+
+        useEffect(() => {
+            axios
+                .get(`http://localhost:3000/properties/address/` + address, {
+                    mode: 'cors',
+                })
+                .then((response) => {
+                    setResults(response.data.results)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        }, [])
+
+        if (results) {
+            return (
+                <div>
+                    <h1 className={`${styles.searchResultTitle}`}>
+                        {results?.length} addresses found.
+                    </h1>
+                    <div className={`${styles.searchResultList}`}>
+                        {results?.map((result) => (
+                            <AddressCard
+                                key={result.siteID}
+                                address={result.fullAddress.split(', ')[0]}
+                                city={result.fullAddress.split(', ')[1]}
+                            />
+                        ))}
+                    </div>
+                    <div
+                        className={`${
+                            results?.length > 7
+                                ? styles.scrollMsg
+                                : styles.hiddenScrollMsg
+                        }`}
+                    >
+                        Scroll down to view more results
+                    </div>
+                </div>
+            )
+        }
+    }
+}
+
+SearchResults.propTypes = {
+    searchString: PropTypes.string,
+}
