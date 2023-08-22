7c0f99b75961b9941d7c6a055dd9f58c63da4fea
emilymendelson <emendelson@deloitte.ca>
2023-08-22 11:57:50 -0400

    adding console.log

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 
+console.log(process.env.NEXT_PUBLIC_BE_APP_URL)
+console.log(process.env.BE_APP_URL)
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
