3af86124245c1bbaea52a004065adf8211177b29
emilymendelson <emendelson@deloitte.ca>
2023-08-23 09:22:46 -0400

    removing console.logs

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../styles/globals.css'
 
-console.log(process.env.NEXT_PUBLIC_BE_APP_URL)
-console.log(process.env.BE_APP_URL)
-
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
