5dc52542664a3704e2ef66e384e3480f54e50f64
emilymendelson <emendelson@deloitte.ca>
2023-08-23 16:12:08 -0400

    Adding in role check

--- pages/property-details.js
+++ pages/property-details.js
@@ -1,7 +1,33 @@
 import Dropdown from '../components/Dropdown/index'
 import Content from '../content.json'
 
-export default function PropertyDetails() {
+export default function PropertyDetails(role) {
+    if (role === 'SuperAdmin') {
+        return (
+            <div>
+                <Dropdown
+                    label={Content.managePINDropdown.label}
+                    options={[
+                        {
+                            label: Content.managePINDropdown.recreateOption,
+                            value: 'recreate-pin',
+                            isDisabled: false,
+                        },
+                        {
+                            label: Content.managePINDropdown.expireOption,
+                            value: 'expire-pin',
+                            isDisabled: false,
+                        },
+                        {
+                            label: Content.managePINDropdown.viewOption,
+                            value: 'view-pin',
+                            isDisabled: false,
+                        },
+                    ]}
+                ></Dropdown>
+            </div>
+        )
+    }
     return (
         <div>
             <Dropdown
@@ -15,11 +41,6 @@ export default function PropertyDetails() {
                     {
                         label: Content.managePINDropdown.expireOption,
                         value: 'expire-pin',
-                        isDisabled: false,
-                    },
-                    {
-                        label: Content.managePINDropdown.viewOption,
-                        value: 'view-pin',
                         isDisabled: false,
                     },
                 ]}
