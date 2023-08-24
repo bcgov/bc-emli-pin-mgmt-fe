a5e8f7907b4440c64f19cac223c128b2e696322e
emilymendelson <emendelson@deloitte.ca>
2023-08-24 10:41:50 -0400

    Modal working without unit tests

--- components/Modal/Modal.test.jsx
+++ components/Modal/Modal.test.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable testing-library/prefer-screen-queries */
-/* eslint-disable testing-library/no-node-access */
 import { render } from '@testing-library/react'
 import { composeStories } from '@storybook/testing-react'
 import userEvent from '@testing-library/user-event'
