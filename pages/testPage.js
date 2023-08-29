a29635d7ec1cc39ca5317febcd7386fe5d2500dd
emilymendelson <emendelson@deloitte.ca>
2023-08-29 11:28:57 -0400

    Fixing bug and adding build to pipeline

--- pages/testPage.js
+++ pages/testPage.js
@@ -1,46 +1,15 @@
-import Modal from '../components/Modal/index'
 import { useState } from 'react'
-import { Button } from '../components/Button'
-import PropertyDetailsPage from '../components/Property/PropertyDetailsPage'
+import ManagePINDropdown from '../components/Property/ManagePINDropdown/ManagePINDropdown'
 
 export default function TestPage() {
-    const [isOpen, setIsOpen] = useState(false)
-    function modalFunction() {
-        console.log('modal function')
-    }
-    const env_var = process.env.TEST_VAR
     return (
         <div>
-            {/* <h1>Test Page {env_var}</h1>
-            <Button
-                handleOnClick={() => setIsOpen(true)}
-                variant="primary"
-                size="medium"
-            >
-                Click Me
-            </Button>
-            <Modal
-                modalHeader="Basic Modal"
-                modalId="basicModal"
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
-                modalMainBtn={{
-                    text: 'Close',
-                    size: 'medium',
-                    variant: 'primary',
-                    onClickHandler: () => modalFunction(),
-                }}
-                modalSecondaryBtn={{
-                    id: 'btnCloseModal',
-                    text: 'Close',
-                    size: '',
-                    variant: 'secondary',
-                    onClickHandler: () => modalFunction(),
-                }}
-            >
-                Modal Text
-            </Modal> */}
-            <PropertyDetailsPage></PropertyDetailsPage>
+            <ManagePINDropdown
+                livePinId="1234"
+                expirationReason="OP"
+                expiredByName="John Smith"
+                expiredByUsername="jsmith"
+            />
         </div>
     )
 }
