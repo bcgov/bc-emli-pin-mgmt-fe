605de6e57ce717111c8ef5aac1457f38842b3ea1
Habiba Zaman <>
2023-10-11 22:15:27 -0230

    removed extra env variable

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     reactStrictMode: true,
     output: 'standalone',
     env: {
-        BE_APP_URL: process.env.BE_APP_URL,
         NEXT_PUBLIC_BE_APP_URL: process.env.NEXT_PUBLIC_BE_APP_URL,
     },
 }
