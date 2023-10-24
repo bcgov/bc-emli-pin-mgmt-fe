0632d4c7d874833cc7f555a60f41aa7bd097eea9
ceciliahangzhou <cezhou@deloitte.ca>
2023-10-24 15:12:48 -0400

    style large dropdown

---
+++ assets/svgs/ArrowDownSolid.jsx
@@ -0,0 +1,19 @@
+function ArrowDownPrimary({ fillColor }) {
+    return (
+        <svg 
+            width="20" 
+            height="20" 
+            viewBox="0 0 20 20" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg">
+            <path 
+                fillRule="evenodd" 
+                clipRule="evenodd" 
+                d="M10 14L16 6H4L10 14Z" 
+                fill="#1A1A1A"/>
+        </svg>
+        
+    )
+}
+
+export default ArrowDownPrimary
