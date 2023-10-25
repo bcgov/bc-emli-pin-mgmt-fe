45a8354dcdda12f75db50682bcfdd9016205a886
ceciliahangzhou <cezhou@deloitte.ca>
2023-10-25 12:43:19 -0400

    add email and phone to expire pin modal

--- components/Property/ExpirePINModal/ExpirePINModal.jsx
+++ components/Property/ExpirePINModal/ExpirePINModal.jsx
@@ -3,18 +3,65 @@ import PropTypes from 'prop-types'
 import Modal from '../../Modal'
 import { useState } from 'react'
 import HttpRequest from '../../../apiManager/httpRequestHandler'
+import Textbox from "../../Textbox/index";
+import Styles from './ExpirePINModal.module.css'
 
 export default function ExpirePINModal({
     livePinId,
     setIsOpen,
     isOpen,
+    propertyAddress
 }) {
     const [openExpireSuccessModal, setOpenExpireSuccessModal] = useState()
     const [openExpireFailureModal, setOpenExpireFailureModal] = useState()
+    const [isExpirePINButtonDisabled, setIsExpirePINDisabled] = useState(true)
+    const [phone, setPhoneValue] = useState()
+    const [email, setEmailValue] = useState()
 
-    function expirePIN() {
+    const setPhoneValueOnChange = (phoneValue) => {
+        setPhoneValue(phoneValue)
+        if (validatePhoneRegex(phoneValue) || validateEmailRegex(email)){
+            setIsExpirePINDisabled(false)
+        } else {
+            setIsExpirePINDisabled(true)
+        }
+    }
+
+    const setEmailValueOnChange = (emailValue) => {
+        setEmailValue(emailValue)
+        if (validatePhoneRegex(phone) || validateEmailRegex(emailValue)){
+            setIsExpirePINDisabled(false)
+        } else {
+            setIsExpirePINDisabled(true)
+        }
+    }
+
+    const validatePhoneRegex = (phone) => {
+        if (/[0-9]{10}/.test(phone)){
+            setPhoneValue(phone)
+            return true
+        }
+        return false
+    }
+
+    const validateEmailRegex = (email) => {
+        if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return true;
+        }
+        return false;
+    }
+
+    const expirePIN = () => {
+        let formattedPhone
+        if (phone) {
+            (phone?.length == 11) ? formattedPhone = phone : formattedPhone = '1' + phone;
+        }
+
         HttpRequest.expirePIN({
+            phoneNumber: formattedPhone,
+            email: email,
             livePinId: livePinId,
+            propertyAddress: propertyAddress,
             expirationReason: Content.pinHistoryModal.typeCode.callCenter,
             // TO DO: to be removed after backend intergrated 
             expiredByUsername: "First last name",
@@ -40,6 +87,7 @@ export default function ExpirePINModal({
                     text: `${Content.expirePINConfirmationModal.primaryButton}`,
                     size: 'medium',
                     variant: 'primary',
+                    disabled: isExpirePINButtonDisabled,
                     onClickHandler: () => expirePIN(),
                 }}
                 modalSecondaryBtn={{
@@ -49,7 +97,31 @@ export default function ExpirePINModal({
                     onClickHandler: () => setIsOpen(false),
                 }}
             >
-                {Content.expirePINConfirmationModal.body}
+                <div className={`${Styles.contentWrap}`}>
+                    {Content.expirePINConfirmationModal.body}
+                </div>
+                <div className='flex justify-between'>
+                    <div className={`${Styles.inputWrap}`}>
+                        <Textbox
+                            textBoxId='phone'
+                            textBoxLabel={Content.regeneratePINModal.phone}
+                            textBoxAriaLabel={Content.regeneratePINModal.phone}
+                            textBoxPlaceholder={Content.regeneratePINModal.phonePlaceHolder}
+                            inputType="tel"
+                            onHandleChange={setPhoneValueOnChange}
+                        />
+                    </div>
+                    <div className={`${Styles.inputWrap}`}>
+                        <Textbox
+                            textBoxId='email'
+                            textBoxLabel={Content.regeneratePINModal.email}
+                            textBoxAriaLabel={Content.regeneratePINModal.email}
+                            textBoxPlaceholder={Content.regeneratePINModal.emailPlaceHolder}
+                            inputType="email"
+                            onHandleChange={setEmailValueOnChange}
+                        />
+                    </div>
+                </div>
             </Modal>
 
             <Modal
