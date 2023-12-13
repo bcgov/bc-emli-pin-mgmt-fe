1de946061f5022ba7d2fca529dd4b12d64ac9c82
emilymendelson <emendelson@deloitte.ca>
2023-12-13 16:55:56 -0500

    Fixing zap issues

--- next.config.js
+++ next.config.js
@@ -9,9 +9,38 @@ const nextConfig = {
         NEXT_PUBLIC_BE_APP_URL: process.env.NEXT_PUBLIC_BE_APP_URL,
     },
     i18n: {
-        locales: ["en"],
-        defaultLocale: "en",
-    }
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: '',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
