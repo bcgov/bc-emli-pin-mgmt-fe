3b3a3ffa9b1cb50c2f8f34fd6feabafa3fda2566
emilymendelson <emendelson@deloitte.ca>
2024-01-18 12:30:17 -0500

    Access code casing

--- components/Property/ManagePINDropdown/ManagePINDropdown.test.jsx
+++ components/Property/ManagePINDropdown/ManagePINDropdown.test.jsx
@@ -16,7 +16,7 @@ describe('<ManagePINDropdown />', () => {
     it('should render properly with View PIN option', async () => {
         const { getByTestId, findByText } = render(<SuperAdminTemplate />)
         userEvent.click(getByTestId('dropdown-btn'))
-        expect(await findByText('View Access Code')).toBeVisible()
+        expect(await findByText('View access code')).toBeVisible()
     })
 
     it('should render properly without View PIN option', async () => {
@@ -24,7 +24,7 @@ describe('<ManagePINDropdown />', () => {
             <PrimaryTemplate />
         )
         userEvent.click(getByTestId('dropdown-btn'))
-        const viewPINOption = screen.queryByText('View Access Code')
+        const viewPINOption = screen.queryByText('View access code')
         expect(viewPINOption).not.toBeInTheDocument()
     })
 })
