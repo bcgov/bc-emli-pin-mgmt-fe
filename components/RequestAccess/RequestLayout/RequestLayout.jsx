cccaca79563bab16a1f95861d7dd375f30c65f24
emilymendelson <emendelson@deloitte.ca>
2024-01-25 11:02:26 -0500

    Content changes for request access page

--- components/RequestAccess/RequestLayout/RequestLayout.jsx
+++ components/RequestAccess/RequestLayout/RequestLayout.jsx
@@ -3,24 +3,44 @@ import Content from '../../../assets/content/content.json'
 import AccessRequest from '../../../assets/svgs/AccessRequest'
 import PageBanner from '../../PageBanner'
 import RequestForm from '../RequestForm'
+import { useState } from 'react';
 
 export default function RequestLayout(props) {
     const {
       userInfo
     } = props;
 
+    const [showForm, setShowForm] = useState(true)
+
+    function handleShowForm(childData) {
+      setShowForm(childData)
+    }
+
     return (
       <div className={`${styles.container}`}>
         <div className="banner-section">
-          <PageBanner
-            title={Content.requestAccessBanner.title}
-            description={Content.requestAccessBanner.description}>
+          {
+            showForm && 
+            <PageBanner
+              title={Content.requestAccessBanner.title}
+              description={Content.requestAccessBanner.description}>
               <AccessRequest/>
             </PageBanner>
+          }
+          {
+            !showForm && 
+            <PageBanner
+              title={Content.requestAccessBanner.title}
+              description={""}>
+              <AccessRequest/>
+            </PageBanner>
+          }
         </div>
         <div className={`${styles.formSection}`}>
             <RequestForm
               userInfo={userInfo}
+              setShowForm={handleShowForm}
+              showForm={showForm}
             />
         </div>
       </div>
